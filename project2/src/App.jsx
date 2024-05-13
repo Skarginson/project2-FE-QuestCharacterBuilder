548563471a3917cf331ff61ef18f05bdb2aae2b3
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-05-13 12:29:07 +0200

    retrieve baseData and pass it to components

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,24 +1,45 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 // import About from "./pages/About";
-// import Create from "./pages/Create";
+import Create from "./pages/Create";
 // import Details from "./pages/Details";
 // import Rules from "./pages/Rules";
 import Homepage from "./pages/Homepage";
-import { BrowserRouter } from "react-router-dom";
+import { API_BASE_URL } from "../consts";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [baseData, setBaseData] = useState([]);
+  const [errorMsg, setErrorMsg] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/data.json`);
+        setBaseData(response.data);
+      } catch (err) {
+        setErrorMsg(err.message);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          {/* <Route path="/About" element={<About />} />
-        <Route path="/Create" element={<Create />} />
+      <Routes>
+        <Route
+          path="/"
+          element={<Homepage baseData={baseData} setBaseData={setBaseData} />}
+        />
+        <Route
+          path="/Create"
+          element={<Create baseData={baseData} setBaseData={setBaseData} />}
+        />
+        {/* <Route path="/About" element={<About />} />
         <Route path="/Rules" element={<Rules />} />
-        <Route path="/Details" element={<Details />} /> */}
-        </Routes>
-      </BrowserRouter>
+      <Route path="/Details" element={<Details />} /> */}
+      </Routes>
     </>
   );
 }
