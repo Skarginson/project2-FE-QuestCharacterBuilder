801b73d2c4d6ad16d3e547f4e4465b59f515df36
Skarginson <102185297+Skarginson@users.noreply.github.com>
2024-05-13 10:28:25 +0200

    fixed errors + add BrowserRouter

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,21 +1,24 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import About from "./pages/About";
-import Create from "./pages/Create";
-import Details from "./pages/Details";
-import Rules from "./pages/Rules";
+// import About from "./pages/About";
+// import Create from "./pages/Create";
+// import Details from "./pages/Details";
+// import Rules from "./pages/Rules";
 import Homepage from "./pages/Homepage";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/About" element={<About />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          {/* <Route path="/About" element={<About />} />
         <Route path="/Create" element={<Create />} />
         <Route path="/Rules" element={<Rules />} />
-        <Route path="/Details" element={<Details />} />
-      </Routes>
+        <Route path="/Details" element={<Details />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
